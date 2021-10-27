44350a446456135bd41bacee87871525e177d669
Night-Voyager <18722007@bjtu.edu.cn>
2021-10-27 16:52:30 +0800

    docs: remove useless comments

--- src/java/team/v1ctorl/nebula/servlets/UserRegisterServlet.java
+++ src/java/team/v1ctorl/nebula/servlets/UserRegisterServlet.java
@@ -30,7 +30,6 @@ public class UserRegisterServlet extends HttpServlet {
         dbUtil = new DbUtil();
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
