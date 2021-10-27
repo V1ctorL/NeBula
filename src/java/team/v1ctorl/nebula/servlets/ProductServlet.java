29a6c98d17a07a7fa3849c031a5c0965dcf74948
Night-Voyager <18722007@bjtu.edu.cn>
2021-10-27 12:23:02 +0800

    style: remove unused imports

--- src/java/team/v1ctorl/nebula/servlets/ProductServlet.java
+++ src/java/team/v1ctorl/nebula/servlets/ProductServlet.java
@@ -6,16 +6,12 @@
 package team.v1ctorl.nebula.servlets;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
