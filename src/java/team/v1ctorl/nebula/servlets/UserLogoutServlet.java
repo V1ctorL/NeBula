0b1d69f93a554be4613ba97505132bcc9816a334
Night-Voyager <18722007@bjtu.edu.cn>
2021-10-28 01:31:21 +0800

    feat(servlets): add a servlet to handle users' logout operation
    The session will be invalidated.

---
+++ src/java/team/v1ctorl/nebula/servlets/UserLogoutServlet.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team.v1ctorl.nebula.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author asus
+ */
+@WebServlet("/logout")
+public class UserLogoutServlet extends HttpServlet {
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        
+        response.getWriter().println("You have successfully logged out.");
+    }
+}
